5ee3864a30f2fd98082d2ac986622b1b93ba81fe
Felix Nahrendorf <fritz.apple@gmail.com>
2025-09-01 14:03:02 +0200

    changes for experiments

--- src/dataset/dataset_seed4d.py
+++ src/dataset/dataset_seed4d.py
@@ -30,7 +30,7 @@ from .view_sampler import ViewSampler, ViewSamplerCfg
 from .dataset_readers import readPixelSplatCamera
 from ..misc.general_utils import img_path_to_Torch, depth_path_to_Torch
 
-SEED4D_DATASET_ROOT = '/app/inputs/seed4d/data/data_baseline_7cams/' 
+SEED4D_DATASET_ROOT = '/app/inputs/seed4d/data/data_7cams_nuscene_adjusted/' 
 assert SEED4D_DATASET_ROOT is not None, "Update the location of the SEED4D Dataset"
 
 LIDAR_DATASET_ROOT = '/app/new/seed4d/pseudo_lidar/' # Will be directory to save pseudo lidar 
