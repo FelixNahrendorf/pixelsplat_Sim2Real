1ad5f6be95602087afd855e7e269a7f5062a44c1
Felix Nahrendorf <fritz.apple@gmail.com>
2025-09-11 13:51:59 +0200

    modified view sampler to perform mixed (ego-ego/ego-exo) training

--- src/dataset/view_sampler/view_sampler_iosplat.py
+++ src/dataset/view_sampler/view_sampler_iosplat.py
@@ -45,7 +45,15 @@ class ViewSamplerIOsplat(ViewSampler[ViewSamplerIOsplatCfg]):
             versus testing modes (target views are kept constant during inference)
             Context views are always set to conditional 6 images on Ego Vehicle
         """
-        temperature = 1
+        temperature = 1 
+
+        ### Explanation on temperature parameter in softmax function:
+        #temperature = 1: Normal softmax behavior, 
+        #temperature > 1 (e.g., 2, 5): Makes the distribution more uniform/smooth
+        #temperature < 1 (e.g., 0.5, 0.1): Makes the distribution sharper/more peaked
+        #temperature → 0: Nearly deterministic selection of only the most similar views
+        #temperature → ∞: Uniform random sampling (ignores similarity entirely)
+
         if self.cfg.num_context_views<6:
             Choice = [0, 1, 2, 3, 4, 5] #Choice = [0, 1, 5, 3, 4, 2]
             start = random.randint(0, len(Choice) - 1)
@@ -53,11 +61,8 @@ class ViewSamplerIOsplat(ViewSampler[ViewSamplerIOsplatCfg]):
         else:
             #index_context = torch.from_numpy(np.array([0, 1, 5, 3, 4, 2])).to(dtype=torch.int64, device=device)
             
-            ###commented out, for testing:
             index_context = torch.from_numpy(np.array([0, 1, 2, 3, 4, 5])).to(dtype=torch.int64, device=device)
             
-            #ego-ego training:
-            # index_context = torch.arange(0, 6, dtype=torch.int64, device=device)
         # #
         # # We will sample only those target views that are 'similar' to the context views
         # # where the similarity is determined by the direction of the cameras
@@ -69,31 +74,33 @@ class ViewSamplerIOsplat(ViewSampler[ViewSamplerIOsplatCfg]):
         target_sample_weight_map = np.array([softmax(temperature*row) for row in context_target_similarity])
         target_sample_weight = np.max(target_sample_weight_map, axis=0)
         target_sample_weight = softmax(temperature*target_sample_weight)
+
         if self.stage=='test' or self.stage=='val':
-            # If the target views are not None, then use them
-            if self.cfg.target_views is not None: 
+            # If the (hardcoded) target views are not None, then use them  
+            if self.cfg.target_views is not None:
                 assert len(self.cfg.target_views) == self.cfg.num_target_views
                 index_target = torch.tensor(self.cfg.target_views, dtype=torch.int64, device=device)
             # If not, then randomly select them
-            else: # perform sanity check first and then create random indexes
-                assert self.cfg.num_target_views<=20 
-                #print('Checkpoint 1111111111111111111111111111111111 target_sample_weight', target_sample_weight, 'len(target_sample_weight)', len(target_sample_weight))
-                
-                ###commented out for ego-ego training
-                #index_target = torch.from_numpy(np.random.choice(np.arange(0, 20), size=self.cfg.num_target_views, 
-                #                                                 replace=False, p=target_sample_weight)).to(dtype=torch.int64) 
-
-                index_target = torch.tensor([0,1,2,3,5,6])
+            elif self.stage=='test': #exo-views
+                #index_target = torch.tensor([0,1,2,3,5,6])
+                index_target = torch.from_numpy(np.random.choice(np.arange(0, 20), size=self.cfg.num_target_views, 
+                                                            replace=False)).to(dtype=torch.int64) 
+            elif self.stage=='val': #ego-views
+                #index_target = torch.tensor([0,1,2,3,5,6])
+                index_target = torch.from_numpy(np.random.choice(np.arange(0, 6), size=self.cfg.num_target_views, 
+                                                            replace=False)).to(dtype=torch.int64) 
         # Otherwise (training) will sample them randomly from 80 views
         elif self.stage == 'train':
-            assert self.cfg.num_target_views<=80
-            # #
-            
-            ###commented out for ego-ego training
+            #assert self.cfg.num_target_views<=80
+            ###ego-exo 
             #index_target = torch.from_numpy(np.random.choice(np.arange(0, 80), size=self.cfg.num_target_views, 
             #                                                 replace=False, p=target_sample_weight)).to(dtype=torch.int64) 
+            ###ego-ego training and mixed ego-ego/ego-exo training (the transform files either have )
+            #index_target = torch.tensor([0,1,2,3,5,6])
+            index_target = torch.from_numpy(np.random.choice(np.arange(0, 98), size=self.cfg.num_target_views, 
+                                                             replace=False)).to(dtype=torch.int64) 
 
-            index_target = torch.tensor([0,1,2,3,5,6])
+
         else: raise KeyError("Called dataset with wrong stage argument ... ")
         
         return index_context, index_target
