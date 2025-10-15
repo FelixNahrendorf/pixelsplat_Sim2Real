f6bdb8235bb533a53d14d09df7d7c94146490550
Felix Nahrendorf <fritz.apple@gmail.com>
2025-10-15 02:13:39 +0200

    included config arguemnt "experiment"

--- src/dataset/view_sampler/view_sampler_iosplat.py
+++ src/dataset/view_sampler/view_sampler_iosplat.py
@@ -36,6 +36,7 @@ class ViewSamplerIOsplat(ViewSampler[ViewSamplerIOsplatCfg]):
         scene,
         extrinsics_context: Float[Tensor, "cview 4 4"],
         extrinsics_target: Float[Tensor, "tview 4 4"],
+        experiment: str = "ego-exo",
         device: torch.device = torch.device("cpu"),
     ) -> tuple[
         Int64[Tensor, " context_view"],  # indices for context views
@@ -76,54 +77,72 @@ class ViewSamplerIOsplat(ViewSampler[ViewSamplerIOsplatCfg]):
         target_sample_weight = softmax(temperature*target_sample_weight)
 
         ###ego-ego/ego-exo mixed training
-        '''if self.stage=='test' or self.stage=='val':
+
+        if experiment == "ego-exo-mixed":
+
+            if self.stage=='test' or self.stage=='val':
+                # If the (hardcoded) target views are not None, then use them  
+                if self.cfg.target_views is not None:
+                    assert len(self.cfg.target_views) == self.cfg.num_target_views
+                    index_target = torch.tensor(self.cfg.target_views, dtype=torch.int64, device=device)
+                # If not, then randomly select them
+
+                ###elif self.stage=='test': #exo-views
+                ###    #index_target = torch.tensor([0,1,2,3,5,6])
+                ###    index_target = torch.from_numpy(np.random.choice(np.arange(0, 6), size=self.cfg.num_target_views, 
+                ###                                                replace=False)).to(dtype=torch.int64)
+
+                else:
+                    assert self.cfg.num_target_views<=20 
+                    index_target = torch.from_numpy(np.random.choice(np.arange(0, 20), size=self.cfg.num_target_views, 
+                                                                    replace=False)).to(dtype=torch.int64)
+                '''elif self.stage=='val': #ego-views
+                    #index_target = torch.tensor([0,1,2,3,5,6])
+                    index_target = torch.from_numpy(np.random.choice(np.arange(0, 6), size=self.cfg.num_target_views, 
+                                                                replace=False)).to(dtype=torch.int64)''' 
+            # Otherwise (training) will sample them randomly from 80 views
+            elif self.stage == 'train':
+                #assert self.cfg.num_target_views<=80
+                ###ego-exo 
+                #index_target = torch.from_numpy(np.random.choice(np.arange(0, 80), size=self.cfg.num_target_views, 
+                #                                                 replace=False, p=target_sample_weight)).to(dtype=torch.int64) 
+                ###ego-ego training and mixed ego-ego/ego-exo training (the transform files either have )
+                #index_target = torch.tensor([0,1,2,3,5,6])
+                index_target = torch.from_numpy(np.random.choice(np.arange(0, 98), size=self.cfg.num_target_views, 
+                                                                replace=False)).to(dtype=torch.int64) 
+
+            else: raise KeyError("Called dataset with wrong stage argument ... ")
+        elif experiment == "ego-exo":
+
+            if self.stage=='test' or self.stage=='val':
+                # If the (hardcoded) target views are not None, then use them  
+                if self.cfg.target_views is not None:
+                    assert len(self.cfg.target_views) == self.cfg.num_target_views
+                    index_target = torch.tensor(self.cfg.target_views, dtype=torch.int64, device=device)
+                else:
+                    assert self.cfg.num_target_views<=20 
+                    index_target = torch.from_numpy(np.random.choice(np.arange(0, 20), size=self.cfg.num_target_views, 
+                                                                    replace=False)).to(dtype=torch.int64)
+            elif self.stage == 'train':
+                index_target = torch.from_numpy(np.random.choice(np.arange(0, 80), size=self.cfg.num_target_views, 
+                                                                replace=False, p=target_sample_weight)).to(dtype=torch.int64) 
+            #if self.stage=='test' or self.stage=='val':
+            #    index_target = torch.from_numpy(np.random.choice(np.arange(0, 6), size=self.cfg.num_target_views, 
+            #                                                    replace=False)).to(dtype=torch.int64)
+            #elif self.stage == 'train':
+                ###ego-exo 
+            #    index_target = torch.from_numpy(np.random.choice(np.arange(0, 80), size=self.cfg.num_target_views, 
+            #                                                    replace=False, p=target_sample_weight)).to(dtype=torch.int64) 
+        elif experiment == "ego-ego":
+
             # If the (hardcoded) target views are not None, then use them  
             if self.cfg.target_views is not None:
                 assert len(self.cfg.target_views) == self.cfg.num_target_views
                 index_target = torch.tensor(self.cfg.target_views, dtype=torch.int64, device=device)
-            # If not, then randomly select them
-            elif self.stage=='test': #exo-views
-                #index_target = torch.tensor([0,1,2,3,5,6])
+            else:
                 index_target = torch.from_numpy(np.random.choice(np.arange(0, 6), size=self.cfg.num_target_views, 
-                                                            replace=False)).to(dtype=torch.int64) 
-            elif self.stage=='val': #ego-views
-                #index_target = torch.tensor([0,1,2,3,5,6])
-                index_target = torch.from_numpy(np.random.choice(np.arange(0, 6), size=self.cfg.num_target_views, 
-                                                            replace=False)).to(dtype=torch.int64) 
-        # Otherwise (training) will sample them randomly from 80 views
-        elif self.stage == 'train':
-            #assert self.cfg.num_target_views<=80
-            ###ego-exo 
-            #index_target = torch.from_numpy(np.random.choice(np.arange(0, 80), size=self.cfg.num_target_views, 
-            #                                                 replace=False, p=target_sample_weight)).to(dtype=torch.int64) 
-            ###ego-ego training and mixed ego-ego/ego-exo training (the transform files either have )
-            #index_target = torch.tensor([0,1,2,3,5,6])
-            index_target = torch.from_numpy(np.random.choice(np.arange(0, 98), size=self.cfg.num_target_views, 
-                                                             replace=False)).to(dtype=torch.int64) 
+                                                                    replace=False)).to(dtype=torch.int64)
 
-
-        else: raise KeyError("Called dataset with wrong stage argument ... ")'''
-
-
-        ##ego-exo training
-        if self.stage=='test' or self.stage=='val':
-            # If the target views are not None, then use them
-            if self.cfg.target_views is not None: 
-                assert len(self.cfg.target_views) == self.cfg.num_target_views
-                index_target = torch.tensor(self.cfg.target_views, dtype=torch.int64, device=device)
-            # If not, then randomly select them
-            else: # perform sanity check first and then create random indexes
-                assert self.cfg.num_target_views<=20 
-                index_target = torch.from_numpy(np.random.choice(np.arange(0, 20), size=self.cfg.num_target_views, 
-                                                                 replace=False)).to(dtype=torch.int64)
-        # Otherwise (training) will sample them randomly from 80 views
-        elif self.stage == 'train':
-            assert self.cfg.num_target_views<=80
-            # #
-            index_target = torch.from_numpy(np.random.choice(np.arange(0, 80), size=self.cfg.num_target_views, 
-                                                             replace=False)).to(dtype=torch.int64)
-        else: raise KeyError("Called dataset with wrong stage argument ... ")
-        
         return index_context, index_target
     
     @property
