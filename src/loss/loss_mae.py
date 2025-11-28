127551457ead647292faa76ae5118f6678668866
Felix Nahrendorf <fritz.apple@gmail.com>
2025-11-29 00:47:40 +0100

    update for nuscenes depth training

--- src/loss/loss_mae.py
+++ src/loss/loss_mae.py
@@ -28,18 +28,18 @@ class LossMae(Loss[LossMaeCfg, LossMaeCfgWrapper]):
         global_step: int,
     ) -> Float[Tensor, ""]:
         # Use the flag from batch - it knows the actual data source!
-        dataset_change = batch.get("dataset_change", False)
+        #dataset_change = batch.get("dataset_change", False)
         
         # Convert Tensor to boolean
-        if isinstance(dataset_change, torch.Tensor):
-            is_nuscene_batch = dataset_change.any().item()
-        else:
-            is_nuscene_batch = bool(dataset_change)
+        #if isinstance(dataset_change, torch.Tensor):
+        #    is_nuscene_batch = dataset_change.any().item()
+        #else:
+        #    is_nuscene_batch = bool(dataset_change)
 
         # Skip depth loss for nuScenes
-        if is_nuscene_batch:
-            print(f"[LOSS DEBUG] [Step {global_step}] ----------- Skipping depth loss for nuScenes -----------------")
-            return torch.tensor(0.0, device=prediction.depth.device, dtype=prediction.depth.dtype)
+        #if is_nuscene_batch:
+        #    print(f"[LOSS DEBUG] [Step {global_step}] ----------- Skipping depth loss for nuScenes -----------------")
+        #    return torch.tensor(0.0, device=prediction.depth.device, dtype=prediction.depth.dtype)
 
         # Depth loss clipped to match ground truth range (0 to 65.535 meters)
         predicted_depth_clipped = torch.clamp(prediction.depth, min=0.0, max=65.535) #example depth=140 set to 65.535 max now to vaoid having big loss there
